cfd199dbd9168e41cc6c6316f59e11a9db4d89a1
Constanza Díaz <cons.diaz@gmail.com>
2026-04-13 22:28:19 -0400

    feat: agregar variables de entorno con python-dotenv

--- src/loader/loader.py
+++ src/loader/loader.py
@@ -1,19 +1,23 @@
 import psycopg2
 from psycopg2.extras import execute_values
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 def conectar_db():
     """
-    Crea y retorna una conexión a PostgreSQL.
+    Crea y retorna una conexión a PostgreSQL
+    usando variables de entorno.
     """
     conn = psycopg2.connect(
-        host="localhost",
-        port=5432,
-        database="pipeline_db",
-        user="postgres",
-        password="admin123"
+        host=os.getenv("DB_HOST"),
+        port=os.getenv("DB_PORT"),
+        database=os.getenv("DB_NAME"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD")
     )
     return conn
-
 
 def cargar_datos(df):
     """
